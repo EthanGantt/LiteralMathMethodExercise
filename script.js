e1ca06e7cc619594f2060469e191903aaee61541
Ethan <ethan.gantt@stu.barren.kyschools.us>
2024-02-02 10:00:17 -0600

    Finished Exercise

---
+++ script.js
@@ -0,0 +1,11 @@
+let warmhugs = "Hi, I\'m Olaf and I like warm hugs."
+console.log(warmhugs);
+console.log(warmhugs.toUpperCase());
+
+console.log(warmhugs.replace("like", "love"));
+
+let beenImpaled = "Oh, look at that. I've been impaled."
+console.log(beenImpaled.slice(18));
+
+let dotDotDot = "..."
+let skullBones = "I don't have a skull...or bones."
