e486caa97198408b72313ae788c63dbdea7b612f
Ethan <ethan.gantt@stu.barren.kyschools.us>
2024-02-02 10:38:40 -0600

    Finished Exercise

--- script.js
+++ script.js
@@ -1,11 +1,18 @@
-let warmhugs = "Hi, I\'m Olaf and I like warm hugs."
+let warmhugs = "Hi, I'm Olaf and I like warm hugs."
 console.log(warmhugs);
 console.log(warmhugs.toUpperCase());
 
 console.log(warmhugs.replace("like", "love"));
+console.log(warmhugs);
 
 let beenImpaled = "Oh, look at that. I've been impaled."
 console.log(beenImpaled.slice(18));
 
 let dotDotDot = "..."
 let skullBones = "I don't have a skull...or bones."
+skullBones = `I don't have a skull${dotDotDot}or bones.`
+console.log(skullBones);
+
+let randomNumber = Math.random() * 3;
+randomNumber = Math.floor(randomNumber) + 1
+console.log(randomNumber);
